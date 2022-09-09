b617437c9351e82d19df5e7d2665575967b6373b
khushal ingle <khushalingle151@gmail.com>
2022-09-09 09:30:35 +0530

    fixed : positive value error

--- hypotenuse.js
+++ hypotenuse.js
@@ -4,20 +4,20 @@ const outputE1 = document.querySelector("#output");
 
 function calculateSumOfSquare(a, b) {
     const sumOfSquare = a * a + b * b;
-    return sumOfSquare
+    return sumOfSquare;
 }
 
 function calculateHypotenuse() {
     const value1 = sides[0].value;
     const value2 = sides[1].value;
-    if (Number(value1) < 0 || Number(value2) < 0) {
+    if (Number(value1) <= 0 || Number(value2) <= 0) {
         outputE1.innerText = "Please enter positive values in the fields.";
     } else if (value1 === "" || value2 === "") {
         outputE1.innerText = "Please enter values in the fields."
-    }else{
-    const sumOfSquare = calculateSumOfSquare(Number(value1), Number(value2));
-    const lengthOfHypotenuse = Math.sqrt(sumOfSquare);
-    outputE1.innerText = "The length of Hypotenuse is " + lengthOfHypotenuse.toFixed(2);
+    } else {
+        const sumOfSquare = calculateSumOfSquare(Number(value1), Number(value2));
+        const lengthOfHypotenuse = Math.sqrt(sumOfSquare);
+        outputE1.innerText = "The length of Hypotenuse is " + lengthOfHypotenuse.toFixed(2);
     }
 }
 
--- index.js
+++ index.js
@@ -10,9 +10,12 @@ function calculateSumOftriangles(angle1, angle2, angle3) {
 function isTriangle() {
   const value1 = inputs[0].value;
   const value2 = inputs[1].value;
-  const value3 = inputs[3].value;
-  if (Number(value1) < 0 || Number(value2) < 0 || Number(value3)) {
+  const value3 = inputs[2].value;
+
+  if (Number(value1) <=0 || Number(value2) <=0 || Number(value3) <=0) {
+
     outputE1.innerText = "Please enter positive values in the fields.";
+
   } else if (value1 === "" || value2 === "" || value3 === "") {
     outputE1.innerText = "Please enter values in the fields.";
   } else {
